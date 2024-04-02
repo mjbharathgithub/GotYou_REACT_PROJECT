19bc8b835192320454b081255d0d95475c8444a2
mjbharathgithub <mjbharath2003@gmail.com>
2024-04-02 09:15:15 +0530

    LAUNCH GOTYOU SCRIPTS

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        GotYou
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
